20189cbd2f5585db540c0e1eca016ae5657ba2a5
Jacob Shulenberger <jacob@builtdigital.io>
2022-12-12 16:01:06 -0600

    wip: day 2 advent of code

--- aoc/2022/day1.js
+++ aoc/2022/day1.js
@@ -1,10 +1,11 @@
-import { input } from './input.js';
+import { day1 } from './input.js';
 
 let elves = input.split(/\n\n/g);
 let mostCarried = {
   amount: 0,
   position: 0
 };
+
 for (let i = 0; i < elves.length; i++) {
     const value = elves[i].split(/\n/g).reduce((acc, x) => acc + parseInt(x), 0);
     const shouldChange = value > mostCarried.amount;
@@ -14,3 +15,17 @@ for (let i = 0; i < elves.length; i++) {
 }
 
 console.log(`amount: ${mostCarried.amount}, position: ${mostCarried.position}`)
+
+let topThree = [0,0,0];
+
+for (let i = 0; i < elves.length; i++) {
+    const value = elves[i].split(/\n/g).reduce((acc, x) => acc + parseInt(x), 0);
+    topThree.sort((a,b) => a - b)
+  const indexToChange = topThree.findIndex((item) => item < value);
+    console.log(`value: ${value}, index: ${indexToChange}, curret top: ${topThree}`)
+  if (indexToChange >= 0) {
+      topThree[indexToChange] = value
+  }
+}
+
+console.log(topThree[0] + topThree[1] + topThree[2])
