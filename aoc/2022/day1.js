8e5f5cd44ee0e29ac408759bb7984fc2538bdbb6
Jacob Shulenberger <jacob@builtdigital.io>
2022-12-12 13:35:24 -0600

    2022 advent of code day one

---
+++ aoc/2022/day1.js
@@ -0,0 +1,16 @@
+import { input } from 'input';
+
+let elves = input.split(/\n\n/g);
+let mostCarried = {
+  amount: 0,
+  position: 0
+};
+for (let i = 0; i < elves.length; i++) {
+    const value = elves[i].split(/\n/g).reduce((acc, x) => acc + parseInt(x), 0);
+    const shouldChange = value > mostCarried.amount;
+    console.log(`shouldChange: ${shouldChange}, value: ${value}, current: ${mostCarried.amount} @ ${mostCarried.position}`)
+    mostCarried.amount = shouldChange ? value : mostCarried.amount;
+    mostCarried.position = shouldChange ? i : mostCarried.position;
+}
+
+console.log(`amount: ${mostCarried.amount}, position: ${mostCarried.position}`)
