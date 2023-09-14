4a426fa2ea7b5209c825f9cb762b57e87d31a01b
Jacob Shulenberger <jacob@builtdigital.io>
2023-09-14 12:27:09 -0500

    refactor: start separating funcitonality

--- gopl/main.go
+++ gopl/main.go
@@ -9,11 +9,7 @@
 package main
 
 import (
-	"image"
 	"image/color"
-	"image/gif"
-	"io"
-	"math"
 	"math/rand"
 	"os"
 )
@@ -26,39 +22,39 @@ import (
 	"time"
 )
 
-//!+main
+// !+main
 var palette = []color.Color{
-  color.White,
-  color.RGBA{0x23, 0x2b, 0x31, 0xff},  // bg_dim       
-  color.RGBA{0x28, 0x30, 0x38, 0xff},  // bg0          
-  color.RGBA{0x2e, 0x37, 0x3f, 0xff},  // bg1          
-  color.RGBA{0x37, 0x41, 0x49, 0xff},  // bg2          
-  color.RGBA{0x40, 0x4b, 0x54, 0xff},  // bg3          
-  color.RGBA{0x47, 0x52, 0x5c, 0xff},  // bg4          
-  color.RGBA{0x52, 0x65, 0x64, 0xff},  // bg5          
-  color.RGBA{0x51, 0x3c, 0x40, 0xff},  // bg_red       
-  color.RGBA{0x56, 0x39, 0x45, 0xff},  // bg_visual    
-  color.RGBA{0x37, 0x4b, 0x5f, 0xff},  // bg_blue      
-  color.RGBA{0x3c, 0x4c, 0x44, 0xff},  // bg_green     
-  color.RGBA{0x4f, 0x4f, 0x41, 0xff},  // bg_yellow    
-  color.RGBA{0xd0, 0xc9, 0xa1, 0xff},  // fg           
-  color.RGBA{0xda, 0x72, 0x80, 0xff},  // red          
-  color.RGBA{0xed, 0xa1, 0x73, 0xff},  // orange       
-  color.RGBA{0xdb, 0xba, 0x79, 0xff},  // yellow       
-  color.RGBA{0xa8, 0xc1, 0x80, 0xff},  // green        
-  color.RGBA{0x7b, 0xc2, 0x9a, 0xff},  // aqua         
-  color.RGBA{0x78, 0xb1, 0xbd, 0xff},  // blue         
-  color.RGBA{0xd4, 0x91, 0xb5, 0xff},  // purple       
-  color.RGBA{0x73, 0x84, 0x78, 0xff},  // grey0        
-  color.RGBA{0x7f, 0x92, 0x8d, 0xff},  // grey1        
-  color.RGBA{0x95, 0xa8, 0xa2, 0xff},  // grey2        
-  color.RGBA{0x7e, 0xb3, 0x65, 0xff},  // statusline1  
-  color.RGBA{0x78, 0x84, 0x92, 0xff},  // statusline2  
-  color.RGBA{0xe6, 0x82, 0x74, 0xff},  // statusline3  
+	color.White,
+	color.RGBA{0x23, 0x2b, 0x31, 0xff}, // bg_dim
+	color.RGBA{0x28, 0x30, 0x38, 0xff}, // bg0
+	color.RGBA{0x2e, 0x37, 0x3f, 0xff}, // bg1
+	color.RGBA{0x37, 0x41, 0x49, 0xff}, // bg2
+	color.RGBA{0x40, 0x4b, 0x54, 0xff}, // bg3
+	color.RGBA{0x47, 0x52, 0x5c, 0xff}, // bg4
+	color.RGBA{0x52, 0x65, 0x64, 0xff}, // bg5
+	color.RGBA{0x51, 0x3c, 0x40, 0xff}, // bg_red
+	color.RGBA{0x56, 0x39, 0x45, 0xff}, // bg_visual
+	color.RGBA{0x37, 0x4b, 0x5f, 0xff}, // bg_blue
+	color.RGBA{0x3c, 0x4c, 0x44, 0xff}, // bg_green
+	color.RGBA{0x4f, 0x4f, 0x41, 0xff}, // bg_yellow
+	color.RGBA{0xd0, 0xc9, 0xa1, 0xff}, // fg
+	color.RGBA{0xda, 0x72, 0x80, 0xff}, // red
+	color.RGBA{0xed, 0xa1, 0x73, 0xff}, // orange
+	color.RGBA{0xdb, 0xba, 0x79, 0xff}, // yellow
+	color.RGBA{0xa8, 0xc1, 0x80, 0xff}, // green
+	color.RGBA{0x7b, 0xc2, 0x9a, 0xff}, // aqua
+	color.RGBA{0x78, 0xb1, 0xbd, 0xff}, // blue
+	color.RGBA{0xd4, 0x91, 0xb5, 0xff}, // purple
+	color.RGBA{0x73, 0x84, 0x78, 0xff}, // grey0
+	color.RGBA{0x7f, 0x92, 0x8d, 0xff}, // grey1
+	color.RGBA{0x95, 0xa8, 0xa2, 0xff}, // grey2
+	color.RGBA{0x7e, 0xb3, 0x65, 0xff}, // statusline1
+	color.RGBA{0x78, 0x84, 0x92, 0xff}, // statusline2
+	color.RGBA{0xe6, 0x82, 0x74, 0xff}, // statusline3
 }
 
 func main() {
-  //!-main
+	//!-main
 	// The sequence of images is deterministic unless we seed
 	// the pseudo-random number generator using the current time.
 	// Thanks to Randall McPherson for pointing out the omission.
@@ -76,54 +72,6 @@ func main() {
 	}
 	//!+main
 	lissajous(os.Stdout, palette)
-}
-
-func lissajous(out io.Writer, palette color.Palette) {
-  const (
-		cycles  = 4     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 64    // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)
-	
-  freq := rand.Float64() * 3.0 // relative frequency of y oscillator
-  anim := gif.GIF{LoopCount: nframes,}
-
-	phase := 0.0 // phase difference
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-    for x := 0; x < size*2+1; x++ {
-      for y := 0; y < size*2+1; y++ {
-        img.SetColorIndex(x, y, 5)
-      }
-    }
-    var color byte = 18
-    var count = 0
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				color)
-      if count > 499 {
-        if color == 20 {
-          color = 18
-        } else {
-          color++ 
-        }
-        count = 0
-      } else {
-        count++
-      }
-		}
-
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
-	}
-  
-  gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
 
 //!-main
