6c110002bcbd095b54e6e2d5ca641a7b0bcddf33
Jacob Shulenberger <jacob@builtdigital.io>
2024-07-12 15:44:14 -0500

    feat: csharppg, lvl 3 ch solved

--- csharp/csharppg/lvl3/Program.cs
+++ csharp/csharppg/lvl3/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Stay awhile and listen.");
+Console.WriteLine("...");
+Console.WriteLine("Hail and well met!");
+Console.WriteLine("You point, I punch!");
+Console.WriteLine("Let's mosey");
+
+// How many statements can a program contain?
+// As many as the disk can contain.
