b6f0bef58c086dd873949e7183115bda1c698844
Jacob Shulenberger <jacob@builtdigital.io>
2024-07-12 16:17:35 -0500

    feat: csharppg lvl 4 ch 1 solved

---
+++ csharp/csharppg/lvl4/Program.cs
@@ -0,0 +1,15 @@
+﻿/* The Thing Namer 3000 repaired */
+Console.WriteLine("What kind of thing are we talking about?");
+// the thing itself
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// descriptor of the thing
+string b = Console.ReadLine();
+// hilarious adjectival phrase
+string c = "of Doom";
+// version number of thing
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
+
+// What else could make this code more understandable?
+// Name the variables semantically, according to their use
