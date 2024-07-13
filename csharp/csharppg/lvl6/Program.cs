8e07dafa96eb115d8abf26b059c52c26901e902d
Jacob Shulenberger <jacob@builtdigital.io>
2024-07-13 12:32:10 -0500

    feat: csharppg lvl 6 ch 2 solved

--- csharp/csharppg/lvl6/Program.cs
+++ csharp/csharppg/lvl6/Program.cs
@@ -1,67 +1,95 @@
 ﻿// the variable shop
 
-// stringy
+// strings
 string str = "The string type uses double quotes";
 char chr = 'c';
+Console.WriteLine("a char is assigned with single quotes: " + chr);
+Console.WriteLine("a string can be any length of chars. Like: " + str);
 
 // 1 byte: 0b00000000, 0x00
-byte minByte = 0;
-byte maxByte = 255;
+byte numByte = 0;
+Console.WriteLine("the byte type represents the smallest range of numbers with a single byte (8 bits)");
+Console.WriteLine("Minimum byte value: " + numByte);
+numByte = byte.MaxValue;
+Console.WriteLine("Maximum byte value: " + numByte);
 
-sbyte minSbyte = -128;
-sbyte maxSbyte = 127;
+sbyte numSbyte = -128;
+Console.WriteLine("the sbyte type rpresents the same range as a byte but can be signed for negative numbers");
+Console.WriteLine("the minimum sbyte value: " + numSbyte);
+numSbyte = sbyte.MaxValue;
+Console.WriteLine("the maximum sbyte value: " + numSbyte);
 
 // 2 bytes: 0b00000000_00000000, 0x00_00
-short minShort = -32_768;
-short maxShort = 32_767;
+short numShort = short.MinValue;
+Console.WriteLine("the short type uses 2 bytes for representing whole numbers.");
+Console.WriteLine("Minimum short value: " + numShort);
+numShort = short.MaxValue;
+Console.WriteLine("Maximum short value: " + numShort);
 
-ushort minUshort = 0;
-ushort maxUshort = 65_535;
+ushort numUshort = ushort.MinValue;
+Console.WriteLine("the ushort type is an unsigned, 2 byte short representing whole numbers.");
+Console.WriteLine("Minimum ushort value: " + numUshort);
+numUshort = ushort.MaxValue;
+Console.WriteLine("Maximum ushort value: " + numUshort);
 
 // 4 bytes: 0b00000000_00000000_000000000_000000000, 0x00_00_00_00
-int minInt = -2_147_483_648;
-int maxInt = 2_147_483_647;
+int numInt = int.MinValue;
+Console.WriteLine("an int type is a signed 4 byte representation of whole numbers.");
+Console.WriteLine("Minimum int value: " + numInt);
+numInt = int.MaxValue;
+Console.WriteLine("Maximum int value: " + numInt);
 
-uint minUint = 0;
-uint maxUint = 4_294_967_295;
+uint numUint = uint.MinValue;
+Console.WriteLine("a uint type is an unsigned 4 byte representation of whole numbers.");
+Console.WriteLine("Minimum uint value: " + numUint);
+numUint = uint.MaxValue;
+Console.WriteLine("Maximum uint value: " + numUint);
+
 
 // 8 bytes: 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000
-//          0x00_00_00_00_00_00_00_00
-long minLong = -9_223_372_036_854_775_808 ;
-long maxLong = 9_223_372_036_854_775_807;
+//          0x00000000_00000000
+long numLong = long.MinValue;
+Console.WriteLine("a long type is a signed 8 byte representation of whole numbers.");
+Console.WriteLine("Minimum long value: " + numLong);
+numLong = long.MaxValue;
+Console.WriteLine("Maximum long value: " + numLong);
 
-ulong minUlong = 0;
-ulong maxUlong = 18_446_744_073_709_551_615;
+ulong numUlong = ulong.MinValue;
+Console.WriteLine("a ulong type is an unsigned 8 byte representation of floating point numbers.");
+Console.WriteLine("Minimum ulong value: " + numUlong);
+numUlong = ulong.MaxValue;
+Console.WriteLine("Maximum ulong value: " + numUlong);
 
 // floating point numbers
 //             
 // 4 bytes: 0b00000000_00000000_000000000_000000000, 0x00_00_00_00
-float numFloat = 1.123f;
+float numFloat = float.MinValue;
+Console.WriteLine("a float type can represent a wider range of numbers since it reserves 8 of its 32 bits for the exponent (1 bit for the sign, 23 for the mantissa).");
+Console.WriteLine("Minimum float value: " + numFloat);
+numFloat = float.MinValue;
+Console.WriteLine("Maximum float value: " + numFloat);
+
 
 // 8 bytes: 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000
 //          0x00000000_00000000
-double numDouble = 0.0456;
+double numDouble = double.MinValue;
+Console.WriteLine("a double is a 8 byte floating point number that represents both a larger absolute range and greater precision than a float.");
+Console.WriteLine("Minimum double value: " + numDouble);
+numDouble = double.MaxValue;
+Console.WriteLine("Maximum double value: " + numDouble);
+
 
 //             sign & exp  ┌-------- number --------┐ 
 // 16 bytes: 0x 00000000 _ 00000000_00000000_00000000
-decimal numDecimal = 1e11m;
+decimal numDecimal = decimal.MinValue;
+Console.WriteLine("a decimal type represents a smaller absolute range than a float but with much greater precision as 96 of the 128 bits are reserved for the mantissa.");
+Console.WriteLine("Minimum decimal value: " + numDecimal);
+numDecimal = decimal.MaxValue;
+Console.WriteLine("Maximum decimal value: " + numDecimal);
 
+bool boolean = true;
+Console.WriteLine("a bool type represents either the true or false values.");
+Console.WriteLine("initialized and assigned true: " + boolean);
+boolean = false;
+Console.WriteLine("bool assigned false: " + boolean);
 
-Console.WriteLine("a char is assigned with single quotes: " + chr);
-Console.WriteLine("a string can be any length of chars. Like: " + str);
-
-Console.WriteLine("the byte type represents the smallest range of numbers with a single byte (8 bits) from " + minByte + " to " + maxByte);
-Console.WriteLine("the sbyte type rpresents the same range as a byte but can be signed for negative numbers, from " + minSbyte + " to " + maxSbyte);
-
-Console.WriteLine("the short type uses 2 bytes for representing numbers from " + minShort + " to " + maxShort);
-Console.WriteLine("the ushort type is an unsigned short representing numbers from " + minUshort + " to " + maxUshort);
-
-Console.WriteLine("an int type is a signed 4 byte representation of numbers in the range " + minInt + " - " + maxInt);
-Console.WriteLine("an uint type is an unsigned 4 byte representation of numbers in the range " + minUint + " - " + maxUint);
-
-Console.WriteLine("a long type is a signed 8 byte representation of numbers in the range " + minLong + " - " + maxLong);
-Console.WriteLine("a ulong type is an unsigned 8 byte representation of numbers in the range " + minUlong + " - " + maxUlong);
-
-Console.WriteLine("a float type can represent a wider range of numbers since it reserves 8 of its 32 bits for the exponent (1 bit for the sign, 23 for the mantissa): " + numFloat);
-Console.WriteLine("a double is a 8 byte floating point number that represents both a larger absolute range and greater precision than a float: " + numDouble);
-Console.WriteLine("a decimal type represents a smaller absolute range than a float but with much greater precision as 96 of the 128 bits are reserved for the mantissa: " + numDecimal);
