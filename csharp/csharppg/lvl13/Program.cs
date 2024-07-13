b5c8dcdc64f7674f307aca372ff7e1f5067b1d4e
Jacob Shulenberger <jacob@builtdigital.io>
2024-07-13 18:46:30 -0500

    feat: csharppg lvl 13 ch 1 & sq recursion

---
+++ csharp/csharppg/lvl13/Program.cs
@@ -0,0 +1,33 @@
+﻿// ask for number
+
+int AskForNumber(string text)
+{
+  Console.Write(text + " ");
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int AskForNumberInRange(string text, int min, int max) 
+{
+  int result;
+  do {
+    Console.Write(text + " ");
+    result = Convert.ToInt32(Console.ReadLine());
+  }
+  while (result < min || result > max);
+
+  return result;
+}
+
+void Countdown(int number) 
+{
+  Console.WriteLine(number);
+  number -= 1;
+  if (number < 1) {
+    return;
+  } else {
+    Countdown(number);
+  }
+}
+
+Countdown(10);
