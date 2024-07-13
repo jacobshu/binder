5d4395090d58cf3c2a94e518acae7cdaa4e1cb21
Jacob Shulenberger <jacob@builtdigital.io>
2024-07-13 13:02:00 -0500

    feat: csharppg lvl 7 ch 1 solved

---
+++ csharp/csharppg/lvl7/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What is the base of the triangle?");
+float triangleBase = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Good, good. Now what is the height?");
+float triangleHeight = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Yes, yes. Let me think...");
+Thread.Sleep(1000);
+Console.WriteLine("The area of your triangle is " + triangleBase * triangleHeight / 2);
