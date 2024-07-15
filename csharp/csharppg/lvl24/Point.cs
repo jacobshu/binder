c9cffe5e4804cae1c3d16a5b8b69f6d2e998fed6
Jacob Shulenberger <jacob@builtdigital.io>
2024-07-15 08:09:02 -0500

    feat: csharppg lvl 24 ch 1 & 2

---
+++ csharp/csharppg/lvl24/Point.cs
@@ -0,0 +1,13 @@
+class Point
+{
+  public int X { get; set; }
+  public int Y { get; set; }
+
+  public Point(int x, int y)
+  {
+    X = x;
+    Y = y;
+  }
+
+  public Point() => new Point(0, 0);
+}
---
+++ csharp/csharppg/lvl24/Program.cs
@@ -0,0 +1,10 @@
+﻿Point p = new Point(2, 3);
+Point q = new Point(-4, 0);
+Console.WriteLine($"({p.X}, {p.Y})");
+Console.WriteLine($"({q.X}, {q.Y})");
+
+Color a = Color.Red;
+Color b = new Color(64, 128, 255);
+Console.WriteLine($"{a.R}, {a.G}, {a.B}");
+Console.WriteLine($"{b.R}, {b.G}, {b.B}");
+
