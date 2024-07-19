47c278c72902bec040d1a1abd8907f623f455173
Jacob Shulenberger <jacob@builtdigital.io>
2024-07-19 12:28:30 -0500

    wip: csharppg lvl31, debug mode, hazards

--- csharp/csharppg/lvl31/Program.cs
+++ csharp/csharppg/lvl31/Program.cs
@@ -1,4 +1,8 @@
-﻿Game game = new Game(4);
+﻿Console.ForegroundColor = ConsoleColor.Gray;
+Console.Write("How big are the caverns? ");
+int size = Convert.ToUInt16(Console.ReadLine());
+Console.Clear();
+Game game = new Game(size);
 game.Run();
 
 public record Point(int X, int Y);
