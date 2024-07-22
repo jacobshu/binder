3918bd6e156b99cee699dce0c0055ce0fa608984
Jacob Shulenberger <jacob@builtdigital.io>
2024-07-22 16:37:56 -0500

    feat: testing out maui

---
+++ csharp/testapp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace testapp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnDecrementClicked(object sender, EventArgs e)
+	{
+		count--;
+
+    CounterTracker.Text = $"The count is {count}";
+
+		SemanticScreenReader.Announce(CounterTracker.Text);
+	}
+
+  private void OnIncrementClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		CounterTracker.Text = $"The count is {count}";
+
+    SemanticScreenReader.Announce(CounterTracker.Text);
+	}
+
+}
+
