634fe1d23570e313865914ea0949a4a31b6fb682
Jacob Shulenberger <jacob@builtdigital.io>
2024-12-06 11:44:26 -0600

    feat: aoc 2024 day 3 done, annoyingly

--- aoc/2024/js/day03.js
+++ aoc/2024/js/day03.js
@@ -3,9 +3,10 @@ import { fileURLToPath } from 'node:url';
 import * as path from 'node:path';
 import * as fs from 'node:fs'
 import kleur from 'kleur'
+import { argv0 } from 'node:process';
 
 async function processLineByLine() {
-  let inputPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '../day03.txt')
+  let inputPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '../data/day03.txt')
   const fileStream = fs.createReadStream(inputPath);
 
   const reader = rl.createInterface({
@@ -15,23 +16,36 @@ async function processLineByLine() {
 
   let count = 0
   let withMeta = 0
+  let agg = ''
   for await (const line of reader) {
+    agg += line
     count += scan(line)
     let ranges = metaCommand(line)
-    let lastIndex = 0
     ranges.forEach(r => {
-      let slice = line.slice(lastIndex, r.index)
-      const matching = r.do === 'do()' ? slice.slice(0, 4) : slice.slice(0, 7)
-      console.log(r.do === matching ? kleur.bold().green(r.do) : kleur.bold().red(r.do), 'from: ', lastIndex, ' to ', r.index, '\n', slice.slice(0, 6), '\n')
-      if (r.do) {
-        withMeta += scan(slice, false)
+      let slice = line.slice(r.from, r.to)
+      if (r.do === 'do()') {
+        withMeta += scan(slice, true)
       }
-      lastIndex = r.index
     })
   }
 
   console.log(kleur.bold().magenta('mul: ' + count))
   console.log(kleur.bold().magenta('mul with commands: ' + withMeta))
+
+  count = scan(agg)
+  withMeta = metaCommand(agg).reduce((acc, curr) => {
+    if (curr.do) {
+      let s = agg.slice(curr.from, curr.to)
+      acc += scan(s, true)
+    }
+    console.log(curr, acc)
+    return acc
+  }, 0)
+  console.log(kleur.bold().magenta('mul: ' + count))
+  console.log(kleur.bold().magenta('mul with commands: ' + withMeta))
+  console.log(kleur.bold().red('working: ' + (withMeta === 59097164)))
+
+
   // 75466465 too high
   reader.close()
 }
@@ -41,40 +55,18 @@ processLineByLine();
 function metaCommand(txt) {
   const regex = /(do\(\)|don't\(\))/g
   const toggles = txt.matchAll(regex)
-  const commands = []
+  const commands = [{ from: 0, do: true, to: -Infinity }]
   for (const match of toggles) {
-    commands.push({ index: match.index, do: match[0] })
-  }
-  // for (const match of dos) {
-  //   console.log(match)
-  //   commands.push({ index: match.index, do: true })
-  // }
-  // for (const m of donts) {
-  //   commands.push({ index: m.index, do: false })
-  // }
-
-  return commands.sort((a, b) => a.index - b.index)
-  let enabled = true
-  const filtered = commands.filter(c => {
-    let keep = c.do !== enabled
-    enabled = c.do
-    return keep
-  })
-
-  return filtered
-  const enabledRanges = []
-  for (let i = 0; i < filtered.length; i += 2) {
-    if (i === 0) {
-      enabledRanges.push([0, filtered[i].index])
-    } else if (i === filtered.length - 2) {
-      enabledRanges.push([filtered[i - 1].index, filtered[i].index])
-      enabledRanges.push([filtered[i + 1].index, Infinity])
-    } else {
-      enabledRanges.push([filtered[i - 1].index, filtered[i].index])
-    }
+    commands.push({ from: match.index, do: match[0] === 'do()', to: -Infinity })
   }
 
-  return enabledRanges
+  commands.sort((a, b) => a.from - b.from)
+  let lastFrom = Infinity
+  for (let i = commands.length - 1; i >= 0; i--) {
+    commands[i].to = lastFrom
+    lastFrom = commands[i].from
+  }
+  return commands
 }
 
 function scan(txt, debug) {
@@ -93,4 +85,3 @@ function scan(txt, debug) {
   if (debug) console.log(logs)
   return products.reduce((acc, curr) => acc += curr)
 }
-
